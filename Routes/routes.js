8862f560fe7a1eb2ae07aa2da565905b09e5817b
MarianoRamborger <Marianoramborger@gmail.com>
2020-06-17 15:49:33 -0300

    interface bugs fixed

--- Routes/routes.js
+++ Routes/routes.js
@@ -178,8 +178,8 @@ router.post("/int", async (req,res) => {
     if (!comparePassword) { return res.status(400).send("Password incorrecto; cambiar para no dar info de que es el password lo que está mal")}
 
     
-    let reqPath = path.join(__dirname, './../DBint/Int-list.html')
-    console.log(reqPath)
+    let reqPath = path.join(__dirname, './../../DBint/Int-list.html')
+    console.log(`El path es: reqPath`)
     
     res.sendFile(reqPath)
 
